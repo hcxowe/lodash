15a6ee8c3a1f2c58754cc0785865cae9a0792644
hcxowe <hcxowe@126.com>
2018-01-04 17:26:39 +0800

    添加键值对pair到map

--- .internal/addMapEntry.js
+++ .internal/addMapEntry.js
@@ -1,15 +1,30 @@
 /**
- * Adds the key-value `pair` to `map`.
- *
+ * 添加键值对pair到map
+ * 
  * @private
- * @param {Object} map The map to modify.
- * @param {Array} pair The key-value pair to add.
- * @returns {Object} Returns `map`.
+ * @param {Map} map
+ * @param {Array} pair
+ * @returns {Map}
  */
 function addMapEntry(map, pair) {
   // Don't return `map.set` because it's not chainable in IE 11.
+  // 不返回map.set，因为它在IE11是不可链的
   map.set(pair[0], pair[1])
   return map
 }
 
 export default addMapEntry
+
+/**
+ * 为 map 添加以数组存储 key-value 的项
+ */
+
+/* 
+var map   = new Map(),
+    item  = ['name', 'lodash'];
+
+addMapEntry(map, item); // map => name-lodash 
+
+typeof map; // Object
+*/
+ 
