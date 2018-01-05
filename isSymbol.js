a9556556ec8ccf46d5c68ceef2f79aea4a5cf54c
hcxowe <hcxowe@126.com>
2018-01-05 16:49:37 +0800

    检查分类为 Symbol 或者 Object 的 value 是否为 symbol

--- isSymbol.js
+++ isSymbol.js
@@ -3,6 +3,8 @@ import getTag from './.internal/getTag.js'
 /**
  * Checks if `value` is classified as a `Symbol` primitive or object.
  *
+ * 检查分类为 Symbol 或者 Object 的 value 是否为 symbol
+ * 
  * @since 4.0.0
  * @category Lang
  * @param {*} value The value to check.
@@ -21,3 +23,23 @@ function isSymbol(value) {
 }
 
 export default isSymbol
+
+/* 
+// 自定义一个迭代器
+var obj = {
+	[Symbol.iterator]: function() {
+		return {
+			next: function() {
+				return {value: 1, done: false}
+			}
+		}
+    }
+}
+
+var count = 0;
+for(var item of obj) {
+	if(count++ > 10) break
+
+	console.log(item)
+} 
+*/
