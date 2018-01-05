52c1c56b1955907553ab081261905ccdde275ec3
hcxowe <hcxowe@126.com>
2018-01-05 13:35:47 +0800

    检查Node环境下的global变量

--- .internal/freeGlobal.js
+++ .internal/freeGlobal.js
@@ -1,4 +1,7 @@
-/** Detect free variable `global` from Node.js. */
+/** 
+ * Detect free variable `global` from Node.js. 
+ * 检查Node环境下的global变量
+ */
 const freeGlobal = typeof global == 'object' && global !== null && global.Object === Object && global
 
 export default freeGlobal
