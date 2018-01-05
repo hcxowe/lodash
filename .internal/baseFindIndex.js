9e3fac2d8159260d4d2289554e83a80777a01914
hcxowe <hcxowe@126.com>
2018-01-05 10:13:27 +0800

    findIndex 与 findLastIndex 的基本实现

--- .internal/baseFindIndex.js
+++ .internal/baseFindIndex.js
@@ -1,5 +1,6 @@
 /**
  * The base implementation of `findIndex` and `findLastIndex`.
+ * findIndex 与 findLastIndex 的基本实现
  *
  * @private
  * @param {Array} array The array to inspect.
@@ -21,3 +22,33 @@ function baseFindIndex(array, predicate, fromIndex, fromRight) {
 }
 
 export default baseFindIndex
+
+
+/**
+ * Array.find & Array.findIndex 的用法
+ */
+
+/* 
+var ary = [1,2,3,4,5]
+ary.find(x => x > 3) // 4
+ary.findIndex(x => x > 4) // 4 
+
+// find findIndex 从数组0到length-1迭代调用传入函数，如果迭代期间原数组大小改变，将不会影响迭代次数
+ary.find(function(ele, index, ary) {
+  if (index == 1) {
+    ary.length = 3
+  }
+
+  console.log(ele); // 1 2 3 undefined*2 
+})
+
+// find findIndex 当为访问的数组元素发生改变，则访问该元素索引时，获取的是改变之后的值
+ary = [1,2,3,4,5]
+ary.find(function(ele, index) {
+  if (index == 0) {
+    delete ary[3]
+  }
+
+  console.log(ele); // 1 2 3 undefined 5
+}); 
+*/
