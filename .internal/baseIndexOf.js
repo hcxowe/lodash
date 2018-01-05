bb32bbe1e9a114ef18e7c9fc0bdfcea60ee702df
hcxowe <hcxowe@126.com>
2018-01-05 10:47:19 +0800

    indexOf基本实现，未检查fromIndex参数

--- .internal/baseIndexOf.js
+++ .internal/baseIndexOf.js
@@ -4,7 +4,10 @@ import strictIndexOf from './strictIndexOf.js'
 
 /**
  * The base implementation of `indexOf` without `fromIndex` bounds checks.
- *
+ * indexOf基本实现，未检查fromIndex参数
+ * 
+ * 如果不是NaN则进行strictIndexOf查找，如果是NaN则使用baseIsNaN函数进行查找
+ * 
  * @private
  * @param {Array} array The array to inspect.
  * @param {*} value The value to search for.
