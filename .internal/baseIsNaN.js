f7d0b19e0a14940daaa8d3e0abf3117c41f820dc
hcxowe <hcxowe@126.com>
2018-01-05 09:25:10 +0800

    isNaN的基本实现，没有支持Number对象

--- .internal/baseIsNaN.js
+++ .internal/baseIsNaN.js
@@ -1,5 +1,6 @@
 /**
  * The base implementation of `isNaN` without support for number objects.
+ * isNaN的基本实现，没有支持Number对象 
  *
  * @private
  * @param {*} value The value to check.
@@ -10,3 +11,30 @@ function baseIsNaN(value) {
 }
 
 export default baseIsNaN
+
+/* 
+var num = new Number('x');
+num !== num; // false;
+
+var num = Number('x');
+num !== num; // true; 
+*/
+
+/**
+ * 另一个版本实现isNaN
+ * 
+ * @param {any} value 
+ */
+/* 
+function hIsNaN(value) {
+  if (!isNumber(value)) {
+    return false;
+  }
+
+  return +value !== +value;
+}
+
+function isNumber(num) {
+  return typeof num === 'number' || Object.prototype.toString.call(num) === '[object Number]'
+} 
+*/
