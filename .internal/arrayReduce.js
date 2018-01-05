fb5a847fa2455fd709f45a0a092bb299126c48e5
hcxowe <hcxowe@126.com>
2018-01-05 15:15:25 +0800

    Array.reduce 的特定版本

--- .internal/arrayReduce.js
+++ .internal/arrayReduce.js
@@ -1,12 +1,13 @@
 /**
  * A specialized version of `reduce` for arrays.
+ * 
+ * Array.reduce 的特定版本
  *
  * @private
  * @param {Array} [array] The array to iterate over.
  * @param {Function} iteratee The function invoked per iteration.
  * @param {*} [accumulator] The initial value.
- * @param {boolean} [initAccum] Specify using the first element of `array` as
- *  the initial value.
+ * @param {boolean} [initAccum] Specify using the first element of `array` as the initial value.
  * @returns {*} Returns the accumulated value.
  */
 function arrayReduce(array, iteratee, accumulator, initAccum) {
@@ -16,10 +17,17 @@ function arrayReduce(array, iteratee, accumulator, initAccum) {
   if (initAccum && length) {
     accumulator = array[++index]
   }
+
   while (++index < length) {
     accumulator = iteratee(accumulator, array[index], index, array)
   }
+
   return accumulator
 }
 
 export default arrayReduce
+
+/* 
+var ary = [1,2,3,4,5];
+ary.reduce(function(x, y) {return x+y}, 100);  // 115
+*/
