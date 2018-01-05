710ad6f6e4f887bba5692ee797a73c84465b55a6
hcxowe <hcxowe@126.com>
2018-01-05 08:50:38 +0800

    Set的普通使用方式

--- .internal/addSetEntry.js
+++ .internal/addSetEntry.js
@@ -14,3 +14,39 @@ function addSetEntry(set, value) {
 }
 
 export default addSetEntry
+
+/** 
+ * Set的基本使用
+ */
+/* 
+var set = new Set();
+set.add({x: 1});
+set.add({x: 2});
+set.add({x: 3});
+
+console.log('--------------遍历Set------------------');
+for (let item of set) {
+  console.log(item); // { x: 1 }, { x: 2 }, { x: 3 } 
+}
+
+ // entries()不适用与Set
+ // values与keys返回的迭代器是一样的
+var iteVal = set.values(),
+    iteKey = set.keys();
+    iteEnt = set.entries();
+  
+console.log('--------------遍历Set.values()--------------');
+for (let item of iteVal) {
+  console.log(item);
+}
+
+console.log('--------------遍历Set.entries()------------------');
+for (let item of iteEnt) {
+  console.log(item);
+}
+
+console.log('--------------遍历Set.keys()------------------');
+for (let item of iteKey) {
+  console.log(item);
+} 
+*/
