55c5b3d272ecae668ceba504778e32f684bd22f9
hcxowe <hcxowe@126.com>
2018-01-05 09:03:09 +0800

    一个Array.forEach的实现

--- .internal/arrayEach.js
+++ .internal/arrayEach.js
@@ -1,5 +1,6 @@
 /**
  * A specialized version of `forEach` for arrays.
+ * 一个Array.forEach的实现
  *
  * @private
  * @param {Array} [array] The array to iterate over.
@@ -10,12 +11,26 @@ function arrayEach(array, iteratee) {
   let index = -1
   const length = array == null ? 0 : array.length
 
+  // Array.forEach并不会因为传入的函数返回false就中断遍历
   while (++index < length) {
     if (iteratee(array[index], index, array) === false) {
       break
     }
   }
+  // 返回数组本身用于链式调用
   return array
 }
 
 export default arrayEach
+
+/**
+ * Array.forEach使用
+ */
+
+/* 
+var ary = [1,2,3,4,5];
+ary.forEach(function(...rest) {
+	console.log(rest);// 循环五次，并不会因 return false 中断循环
+	return false;
+}); */
+
