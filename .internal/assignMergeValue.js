962b1da8a81d031ad9b1d8eec46fd519a536027f
hcxowe <hcxowe@126.com>
2018-01-05 15:44:01 +0800

    给对象属性设置值: value为undefined时，如果对象没有属性key则不进行赋值分配

--- .internal/assignMergeValue.js
+++ .internal/assignMergeValue.js
@@ -5,6 +5,8 @@ import eq from '../eq.js'
  * This function is like `assignValue` except that it doesn't assign
  * `undefined` values.
  *
+ * 给对象属性设置值: value为undefined时，如果对象没有属性key则不进行赋值分配
+ * 
  * @private
  * @param {Object} object The object to modify.
  * @param {string} key The key of the property to assign.
